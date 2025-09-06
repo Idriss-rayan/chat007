c7bf07871ae146c5773c77711c7763a8fc0a1c68
Idriss-rayan <rayanidriss27@gmail.com>
2025-09-06 23:26:30 +0600

    add new file

--- lib/pages/otherspages/messages/card_msg.dart
+++ lib/pages/otherspages/messages/card_msg.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CardMsg extends StatefulWidget {
   const CardMsg({super.key});
@@ -14,39 +15,41 @@ class _CardMsgState extends State<CardMsg> {
       margin: const EdgeInsets.symmetric(vertical: 8),
       padding: const EdgeInsets.all(12),
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: Color.fromARGB(7, 236, 34, 31),
         borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 6,
-            offset: const Offset(0, 3),
-          ),
-        ],
       ),
       child: Row(
         children: [
-          const CircleAvatar(
-            radius: 24,
-            backgroundColor: Colors.amber,
-            child: Icon(Icons.person, color: Colors.white),
+          SvgPicture.asset(
+            'assets/component/avatar.svg',
+            width: 64,
+            height: 64,
           ),
           const SizedBox(width: 12),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: const [
-                Text("John Doe",
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                Text(
+                  "John Doe",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(195, 0, 0, 0),
+                  ),
+                ),
                 SizedBox(height: 4),
-                Text("Dernier message...",
-                    style: TextStyle(color: Colors.black54)),
+                Text(
+                  "Dernier message...",
+                  style: TextStyle(color: Colors.black54),
+                ),
               ],
             ),
           ),
-          const Text("12:45",
-              style: TextStyle(color: Colors.black45, fontSize: 12)),
+          const Text(
+            "12:45",
+            style: TextStyle(color: Colors.black45, fontSize: 12),
+          ),
         ],
       ),
     );
