d3dac6577f5db2a8692f6c3a63ba1d53ffab74cf
Idriss-rayan <rayanidriss27@gmail.com>
2025-09-06 23:51:46 +0600

    add new file

--- lib/pages/otherspages/messages/card_msg.dart
+++ lib/pages/otherspages/messages/card_msg.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:simplechat/pages/otherspages/messages/chat_discussion.dart';
 
 class CardMsg extends StatefulWidget {
   const CardMsg({super.key});
@@ -11,46 +12,61 @@ class CardMsg extends StatefulWidget {
 class _CardMsgState extends State<CardMsg> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: Color.fromARGB(7, 236, 34, 31),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Row(
-        children: [
-          SvgPicture.asset(
-            'assets/component/avatar.svg',
-            width: 64,
-            height: 64,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                ChatDiscussion(),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return FadeTransition(opacity: animation, child: child);
+            },
           ),
-          const SizedBox(width: 12),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  "John Doe",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(195, 0, 0, 0),
-                  ),
-                ),
-                SizedBox(height: 4),
-                Text(
-                  "Dernier message...",
-                  style: TextStyle(color: Colors.black54),
-                ),
-              ],
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Color.fromARGB(7, 236, 34, 31),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            SvgPicture.asset(
+              'assets/component/avatar.svg',
+              width: 64,
+              height: 64,
             ),
-          ),
-          const Text(
-            "12:45",
-            style: TextStyle(color: Colors.black45, fontSize: 12),
-          ),
-        ],
+            const SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "John Doe",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(195, 0, 0, 0),
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    "Dernier message...",
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                ],
+              ),
+            ),
+            const Text(
+              "12:45",
+              style: TextStyle(color: Colors.black45, fontSize: 12),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/pages/otherspages/messages/chat_discussion.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ChatDiscussion extends StatefulWidget {
+  const ChatDiscussion({super.key});
+
+  @override
+  State<ChatDiscussion> createState() => _ChatDiscussionState();
+}
+
+class _ChatDiscussionState extends State<ChatDiscussion> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("data"),
+      ),
+    );
+  }
+}
