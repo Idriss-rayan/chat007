45eb4971a0c43bcbb9fd9aa8171950839c66a2fe
Idriss-rayan <rayanidriss27@gmail.com>
2025-09-17 22:26:01 +0600

    add new file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:simplechat/gate/informations.dart';
 import 'package:simplechat/gate/login_page.dart';
-import 'package:simplechat/pages/main_page.dart';
-import 'package:simplechat/pages/otherspages/users/card_user2.dart';
-import 'package:simplechat/test/test.dart';
 
 void main() {
   runApp(MyApp());
