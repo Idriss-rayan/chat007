c599ea08ab247909b45590ff7a53e1af81185d4e
Idriss-rayan <rayanidriss27@gmail.com>
2025-09-19 23:11:53 +0600

    add new file

--- lib/pages/otherspages/messages/rooms/card_rooms.dart
+++ lib/pages/otherspages/messages/rooms/card_rooms.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:simplechat/pages/otherspages/messages/chat_discussion.dart';
 import 'package:simplechat/pages/otherspages/messages/rooms/chat_rooms.dart';
 
 class CardRooms extends StatefulWidget {
