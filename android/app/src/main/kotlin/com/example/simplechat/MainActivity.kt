9ce3272baeec63cc2ee43b56852c50b97b8fcc01
Idriss-rayan <rayanidriss27@gmail.com>
2025-09-24 14:02:28 +0600

    solve bug

--- android/app/src/main/kotlin/com/example/simplechat/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/simplechat/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.simplechat
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,14 +7,16 @@ import Foundation
 
 import device_info_plus
 import emoji_picker_flutter
+import file_picker
 import path_provider_foundation
 import shared_preferences_foundation
-import speech_to_text_macos
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
